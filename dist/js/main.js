2d459c666a028c82a61e966e23d70527b732a614
Igor <igor.shiota@gmail.com>
2020-03-10 15:30:54 +0000

    cleaned unused code

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,57 +1,3 @@
-// Select DOM items
-// const menuBtn = document.querySelector(".menu-btn");
-// const menu = document.querySelector(".menu");
-// const menuNav = document.querySelector(".menu-nav");
-// const menuBranding = document.querySelector(".menu-branding");
-// const navItems = document.querySelectorAll(".nav-item");
-
-// Set Initial State of Menu
-// let showMenu = false;
-
-// menuBtn.addEventListener("click", toggleMenu);
-
-// function toggleMenu() {
-//   if (!showMenu) {
-//     menuBtn.classList.add("close");
-//     menu.classList.add("show");
-//     menuNav.classList.add("show");
-//     menuBranding.classList.add("show");
-
-//     navItems.forEach(item => item.classList.add("show"));
-
-//     // Set Menu State
-//     showMenu = true;
-//   } else {
-//     menuBtn.classList.remove("close");
-//     menu.classList.remove("show");
-//     menuNav.classList.remove("show");
-//     menuBranding.classList.remove("show");
-
-//     navItems.forEach(item => item.classList.remove("show"));
-
-//     // Set Menu State
-//     showMenu = false;
-//   }
-// }
-
-
-// Read More toggle button
-
-// jQuery(document).ready(function() {
-//   jQuery("#toggle").click(function() {
-//     var elem = $("#toggle").text();
-//     if (elem == "+ Ok, tell me more!") {
-//       //Stuff to do when btn is in the read more state
-//       jQuery("#toggle").text("Ok, got it!");
-//       jQuery("#hidden-text").slideDown();
-//     } else {
-//       //Stuff to do when btn is in the read less state
-//       jQuery("#toggle").text("+ Ok, tell me more!");
-//       jQuery("#hidden-text").slideUp();
-//     }
-//   });
-// });
-
 
 // Multi Modals
 
@@ -59,8 +5,10 @@
 var modalparent = document.getElementsByClassName("modal_multi");
 
 // Get the button that opens the modal
-
 var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
+
+// Get the button that opens the modal
+var modal_btn_contact = document.getElementsByClassName("contact_modal");
 
 // Get the <span> element that closes the modal
 var span_close_multi = document.getElementsByClassName("close_multi");
@@ -74,8 +22,6 @@ function setDataIndex() {
         span_close_multi[i].setAttribute('data-index', i);
     }
 }
-
-
 
 for (i = 0; i < modal_btn_multi.length; i++) {
     modal_btn_multi[i].onclick = function() {
@@ -101,3 +47,6 @@ window.onclick = function(event) {
         modalparent[event.target.getAttribute('data-index')].style.display = "none";
     }
 };
+
+
+  
